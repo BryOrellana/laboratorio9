2cb3e41795af37b6d59d69d3fb100ead12d28cef
BryOrellana <00049318@uca.edu.sv>
2019-11-04 12:30:33 -0600

    Updates

---
+++ precache-manifest.ae176fd35fa3f8cfd2512fba237c474a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5ea7a2da1e98ab97a8825ae7c34a1e",
+    "url": "/BryOrellana/laboratorio9/index.html"
+  },
+  {
+    "revision": "10a936140f5ad0d8e109",
+    "url": "/BryOrellana/laboratorio9/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e07aadc397ca3d9b8048",
+    "url": "/BryOrellana/laboratorio9/static/js/2.7cfc8e13.chunk.js"
+  },
+  {
+    "revision": "10a936140f5ad0d8e109",
+    "url": "/BryOrellana/laboratorio9/static/js/main.2b30f17c.chunk.js"
+  },
+  {
+    "revision": "93817f35ad0f6bd58ead",
+    "url": "/BryOrellana/laboratorio9/static/js/runtime-main.295c438e.js"
+  }
+]);
